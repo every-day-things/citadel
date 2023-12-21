80d5bb288c0c30ff4746fa1dd2114b949cc304f4
Phil Denhoff <phil@denhoff.ca>
2023-12-20 19:16:31 -0800

    feat: add book titles to cover view

--- src/components/molecules/CoverView.svelte
+++ src/components/molecules/CoverView.svelte
@@ -3,6 +3,9 @@
 
   export let bookList: CalibreBook[];
   export let coverPathForBook: (book: CalibreBook) => string;
+
+  const shortenToXChars = (str: string, x: number) =>
+    str.length > x ? str.slice(0, x) + "..." : str;
 </script>
 
 <div class="covers">
@@ -11,6 +14,8 @@
       {#if book.has_cover}
         <!-- svelte-ignore a11y-missing-attribute -->
         <img src={coverPathForBook(book)} />
+        <span class="title">{shortenToXChars(book.title, 50)}</span>
+        <span class="authors">{book.authors}</span>
       {/if}
     </div>
   {/each}
@@ -19,14 +24,15 @@
 <style>
   .covers {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
     gap: 20px;
   }
 
   .book {
     display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: start;
     width: 100%;
   }
 
@@ -34,5 +40,17 @@
     grid-area: "cover";
     max-width: 100%;
     max-height: 400px;
+  }
+
+  span {
+    text-align: center;
+  }
+
+  span.title {
+    color: var(--text-primary);
+    margin: 8px 0;
+  }
+  span.authors {
+    color: var(--text-secondary);
   }
 </style>
