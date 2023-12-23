849294587e6fd6d254b2f4b55a91c5c2be1354d5
Phil Denhoff <phil@denhoff.ca>
2023-12-22 23:17:15 -0800

    feat: do not crash if author already exists

--- src-tauri/src/libs/calibre/add_book.rs
+++ src-tauri/src/libs/calibre/add_book.rs
@@ -1,4 +1,5 @@
 use chrono::{DateTime, NaiveDateTime, NaiveTime, Utc};
+use diesel::sql_types::Integer;
 use std::io::Error;
 
 use diesel::prelude::*;
@@ -13,6 +14,45 @@ pub struct InsertedBook {
     pub book_id: i32,
     pub book_uuid: String,
     pub author_ids: Vec<i32>,
+}
+
+fn upsert_authors(conn: &mut diesel::SqliteConnection, authors: &Vec<String>) -> Vec<i32> {
+    let existing_authors = authors::table
+        .select(Author::as_select())
+        .filter(authors::name.eq_any(authors))
+        .load::<Author>(conn)
+        .expect("Error loading existing authors");
+    let existing_authors_names = existing_authors
+        .iter()
+        .map(|a| a.name.clone())
+        .collect::<Vec<String>>();
+
+    let missing_authors = authors
+        .iter()
+        .filter(|author| !existing_authors_names.contains(author))
+        .collect::<Vec<&String>>();
+
+    let mut author_ids: Vec<i32> = existing_authors.iter().map(|a| a.id.unwrap()).collect();
+    for author in missing_authors {
+        let new_author = Author {
+            id: None, // Set on Insert
+            name: author.clone(),
+            sort: None, // TODO: Call new "authors sort" fn here
+            link: "".to_string(),
+        };
+        let result = diesel::insert_into(authors::table)
+            .values(&new_author)
+            .get_result::<Author>(conn);
+        match result {
+            Ok(author) => {
+                author_ids.push(author.id.unwrap());
+            }
+            Err(e) => {
+                println!("Error inserting author: {}", e);
+            }
+        }
+    }
+    author_ids
 }
 
 pub fn insert_book_metadata(
@@ -50,21 +90,19 @@ pub fn insert_book_metadata(
         .get_result(conn)
         .unwrap();
 
-    let new_author = Author {
-        id: None, // Set on Insert
-        name: author_str.clone(),
-        sort: None, // TODO: Call "authors sort" fn here
-        link: "".to_string(),
-    };
-    let author_inserted = diesel::insert_into(authors::dsl::authors)
-        .values(&new_author)
-        .returning(Author::as_returning())
-        .get_result(conn)
-        .unwrap();
+    // Get inserted book UUID
+    let book_uuid = books::table
+        .select(books::uuid)
+        .filter(books::id.eq(book_inserted.id.unwrap()))
+        .first::<Option<String>>(conn)
+        .expect("Error getting book UUID");
+
+    let new_authors = upsert_authors(conn, &vec![author_str.clone()]);
+    let author_id = new_authors[0];
 
     let new_book_author_link = BookAuthorLink {
         id: None, // Set on Insert
-        author: author_inserted.id.unwrap(),
+        author: author_id,
         book: book_inserted.id.unwrap(),
     };
     diesel::insert_into(books_authors_link::dsl::books_authors_link)
@@ -90,7 +128,7 @@ pub fn insert_book_metadata(
 
     Ok(InsertedBook {
         book_id: book_inserted.id.unwrap(),
-        book_uuid: book_inserted.uuid.unwrap(),
-        author_ids: vec![author_inserted.id.unwrap()],
+        book_uuid: book_uuid.unwrap(),
+        author_ids: vec![author_id],
     })
 }
