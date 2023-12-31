70c316d6585671fa107dce6b27753568b10a66c3
Phil Denhoff <phil@denhoff.ca>
2023-12-30 23:56:40 -0800

    extract tauri backend setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -65,36 +65,40 @@ async fn run_http_server(args: &Vec<String>) -> std::io::Result<()> {
     .await
 }
 
+fn run_tauri_backend() -> std::io::Result<()> {
+    let specta_builder = {
+        let specta_builder = tauri_specta::ts::builder().commands(tauri_specta::collect_commands![
+            libs::calibre::calibre_load_books_from_db,
+            libs::calibre::get_importable_file_metadata,
+            libs::calibre::check_file_importable,
+            libs::calibre::add_book_to_db_by_metadata,
+            libs::calibre::update_book,
+            libs::calibre::init_client,
+            libs::devices::add_book_to_external_drive,
+        ]);
+
+        #[cfg(debug_assertions)] // <- Only export on non-release builds
+        let specta_builder = specta_builder.path("../src/bindings.ts");
+
+        specta_builder.into_plugin()
+    };
+
+    Ok(tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(specta_builder)
+        .plugin(tauri_plugin_persisted_scope::init())
+        .plugin(tauri_plugin_drag::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application"))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
+
     if is_server(&args) {
         run_http_server(&args).await
     } else {
-        let specta_builder = {
-            let specta_builder =
-                tauri_specta::ts::builder().commands(tauri_specta::collect_commands![
-                    libs::calibre::calibre_load_books_from_db,
-                    libs::calibre::get_importable_file_metadata,
-                    libs::calibre::check_file_importable,
-                    libs::calibre::add_book_to_db_by_metadata,
-                    libs::calibre::update_book,
-                    libs::calibre::init_client,
-                    libs::devices::add_book_to_external_drive,
-                ]);
-
-            #[cfg(debug_assertions)] // <- Only export on non-release builds
-            let specta_builder = specta_builder.path("../src/bindings.ts");
-
-            specta_builder.into_plugin()
-        };
-
-        Ok(tauri::Builder::default()
-            .invoke_handler(tauri::generate_handler![greet])
-            .plugin(specta_builder)
-            .plugin(tauri_plugin_persisted_scope::init())
-            .plugin(tauri_plugin_drag::init())
-            .run(tauri::generate_context!())
-            .expect("error while running tauri application"))
+        run_tauri_backend()
     }
 }
