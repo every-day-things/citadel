43adc5c017c8b5cae4680cbdf04dc2791894b9ff
Phil Denhoff <phil@denhoff.ca>
2024-01-10 22:31:33 -0800

    citadel: choose book sort order

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,18 @@
   import { books } from "../stores/books";
   import { any } from "$lib/any";
 
+  const LibraryBookSortOrder = {
+    nameAz: "name-asc",
+    nameZa: "name-desc",
+    authorAz: "author-asc",
+    authorZa: "author-desc",
+  } as const;
+  type LibraryBookSortOrder =
+    | "name-asc"
+    | "name-desc"
+    | "author-asc"
+    | "author-desc";
+
   const x = (event: DragEvent, book: bindings.LibraryBook) => {
     event.preventDefault();
     const coverImageAbsPath = $libraryClient.getCoverPathForBook(
@@ -31,15 +43,37 @@
   };
 
   let view: "table" | "cover" = "cover";
+  let sortOrder = writable<LibraryBookSortOrder>(LibraryBookSortOrder.authorAz);
   let search = writable("");
-  let selectedBooks = derived([books, search], ([$books, search]) =>
-    $books.filter((book) =>
-      search.length === 0
-        ? $books
-        : any(book.author_list, (item) =>
-            item.toLowerCase().includes(search.toLowerCase())
-          ) || book.title.toLowerCase().includes(search.toLowerCase())
-    )
+  let selectedBooks = derived(
+    [books, search, sortOrder],
+    ([$books, search, $sortOrder]) =>
+      $books
+        .filter((book) =>
+          search.length === 0
+            ? $books
+            : any(book.author_list, (item) =>
+                item.toLowerCase().includes(search.toLowerCase())
+              ) || book.title.toLowerCase().includes(search.toLowerCase())
+        )
+        .filter((book) => book.title !== "" && book.author_list.length > 0)
+        .toSorted((a, b) => {
+          const a_author = a.author_list.length > 0 ? a.author_list[0] : "";
+          const b_author = b.author_list.length > 0 ? b.author_list[0] : "";
+
+          switch ($sortOrder) {
+            case "name-asc":
+              return a.title.localeCompare(b.title);
+            case "name-desc":
+              return b.title.localeCompare(a.title);
+            case "author-asc":
+              return a_author.localeCompare(b_author);
+            case "author-desc":
+              return b_author.localeCompare(a_author);
+            default:
+              return 0;
+          }
+        })
   );
   let range = derived(selectedBooks, ($selectedBooks) =>
     $selectedBooks.length === 0 ? "0" : `1-${$selectedBooks.length}`
@@ -86,7 +120,11 @@
 <section class="scrollable-section">
   <div class="books">
     <div class="controls">
-      <input type="text" bind:value={$search} placeholder="Search book titles and authors" />
+      <input
+        type="text"
+        bind:value={$search}
+        placeholder="Search book titles and authors"
+      />
       <div class="switch">
         <button
           on:click={() => (view = "table")}
@@ -97,8 +135,16 @@
           class={view === "cover" ? "selected" : ""}>Covers</button
         >
       </div>
+      <select bind:value={$sortOrder}>
+        <option value={LibraryBookSortOrder.nameAz}>Name (A-Z)</option>
+        <option value={LibraryBookSortOrder.nameZa}>Name (Z-A)</option>
+        <option value={LibraryBookSortOrder.authorAz}>Author (A-Z)</option>
+        <option value={LibraryBookSortOrder.authorZa}>Author (Z-A)</option>
+      </select>
     </div>
-    <span class="num_items">Showing {$range} of {$selectedBooks.length} items</span>
+    <span class="num_items"
+      >Showing {$range} of {$selectedBooks.length} items</span
+    >
     {#if view === "cover"}
       <CoverView bookList={$selectedBooks} dragHandler={x} />
     {:else if view === "table"}
