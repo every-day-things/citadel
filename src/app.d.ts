0d4f8c4f1ab61792a83de06c575bc452612513bb
Phil Denhoff <phil@denhoff.ca>
2024-01-17 16:35:46 -0800

    pkgs: add unplugin for Framework7 icons

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,5 @@
+import 'unplugin-icons/types/svelte';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
+import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    Icons({
+      compiler: "svelte",
+    }),
+  ],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
   },
