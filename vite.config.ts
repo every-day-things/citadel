4c8e13304f60c0cb202972f65aff280946708fdb
Phil Denhoff <phil@denhoff.ca>
2024-03-26 22:05:45 -0700

    feat: import a combo of create-tauri-app and create-vite
    both using React + TS. Uses SWC and ESLint.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 import Icons from "unplugin-icons/vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [
-    sveltekit(),
-    Icons({
-      compiler: "svelte",
-    }),
+    react(),
+    Icons(),
   ],
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  server: {
+    port: 1420,
+    // 2. tauri expects a fixed port, fail if that port is not available
+    strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
   },
