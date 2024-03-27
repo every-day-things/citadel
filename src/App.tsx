945ff7c5d4b61a5eed081416fcd09d384c5ba756
Phil Denhoff <phil@denhoff.ca>
2024-03-26 23:15:56 -0700

    Remove template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import {
   createTheme,
@@ -190,15 +187,8 @@ const theme = createTheme({
 });
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
   const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
 
   return (
     <MantineProvider theme={theme}>
@@ -235,36 +225,6 @@ function App() {
           </AppShell.Navbar>
 
           <AppShell.Main>
-            <div className="row">
-              <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-                <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-              </a>
-              <a href="https://tauri.app" target="_blank" rel="noreferrer">
-                <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-              </a>
-              <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-                <img src={reactLogo} className="logo react" alt="React logo" />
-              </a>
-            </div>
-
-            <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-            <form
-              className="row"
-              onSubmit={(e) => {
-                e.preventDefault();
-                greet();
-              }}
-            >
-              <input
-                id="greet-input"
-                onChange={(e) => setName(e.currentTarget.value)}
-                placeholder="Enter a name..."
-              />
-              <button type="submit">Greet</button>
-            </form>
-
-            <p>{greetMsg}</p>
           </AppShell.Main>
         </div>
       </AppShell>
