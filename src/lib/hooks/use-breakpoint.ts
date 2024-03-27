e791f9c0e36858c0e15042a14cf03cd3cf2ee135
Phil Denhoff <phil@denhoff.ca>
2024-03-26 23:17:40 -0700

    feat: Add hook to figure out if breakpoint satisfied

---
+++ src/lib/hooks/use-breakpoint.ts
@@ -0,0 +1,23 @@
+import { useMantineTheme } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+
+export function useBreakpoint(minBreakpoint = 'sm', exact = false) {
+  const theme = useMantineTheme();
+
+  // biome-ignore lint/suspicious/noImplicitAnyLet: <explanation>
+let  query;
+  if (exact) {
+    // Exact breakpoint: min-width of this breakpoint and max-width of the next one (if it exists)
+    const nextBreakpointIndex = Object.keys(theme.breakpoints).indexOf(minBreakpoint) + 1;
+    const nextBreakpointKey = Object.keys(theme.breakpoints)[nextBreakpointIndex];
+    const maxQuery = nextBreakpointKey ? ` and (max-width: calc(${theme.breakpoints[nextBreakpointKey]} - 1px)` : '';
+    query = `(min-width: ${theme.breakpoints[minBreakpoint]}) ${maxQuery}`;
+  } else {
+    // Min-width query for "minBreakpoint or larger"
+    query = `(min-width: ${theme.breakpoints[minBreakpoint]})`;
+  }
+
+  console.log(query);
+
+  return useMediaQuery(query);
+}
