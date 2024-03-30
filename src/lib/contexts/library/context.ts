63ec700e7cfcd1c5c996376a17faec5157fc01ac
Phil Denhoff <phil@denhoff.ca>
2024-03-30 00:41:48 -0700

    Change Library context to use state enum for runtime safety

--- src/lib/contexts/library/context.ts
+++ src/lib/contexts/library/context.ts
@@ -1,18 +1,53 @@
 import { createContext } from "react";
 import { Library } from "../../library/_types";
 
-export type LibraryContextType = {
-  library: Library;
-  loading: false;
-  error: Error | null;
-} | {
-  library: null;
-  loading: boolean;
-  error: Error | null
+export enum LibraryState {
+	ready = 0,
+	loading = 1,
+	error = 2,
+	closed = 3,
+	starting = 4,
 }
 
-export const LibraryContext = createContext<LibraryContextType>({
-  loading: true,
-  library: null,
-  error: null
-});
+export type LibraryContextType =
+	| {
+			library: null;
+			loading: false;
+			error: Error | null;
+			state: LibraryState.closed;
+		}
+	| {
+			library: null;
+			loading: false;
+			error: null;
+			state: LibraryState.starting;
+		}
+	| {
+			library: Library;
+			loading: false;
+			error: null;
+			state: LibraryState.ready;
+		}
+	| {
+			library: null;
+			loading: true;
+			error: null;
+			state: LibraryState.loading;
+		}
+	| {
+			library: null;
+			loading: false;
+			error: Error;
+			state: LibraryState.error;
+		};
+
+export const DEFAULT_CONTEXT_VALUE: LibraryContextType = {
+	library: null,
+	loading: false,
+	error: null,
+	state: LibraryState.starting
+};
+
+export const LibraryContext = createContext<LibraryContextType>(
+	DEFAULT_CONTEXT_VALUE,
+);
