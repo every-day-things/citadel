78738cee01e06d73d87cfcaf4f335e7f2b7a7318
Phil Denhoff <phil@denhoff.ca>
2024-03-31 22:54:30 -0700

    config: include Mantine styles in Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,6 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 	addons: [
-		"@storybook/addon-onboarding",
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@chromatic-com/storybook",
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,11 @@
 import type { Preview } from '@storybook/react'
 
-import "../src/routes/styles.css"
-import "../src/routes/semantic.css"
+// import "../src/routes/styles.css"
+// import "../src/routes/semantic.css"
+import "../src/styles.css";
+import "@mantine/core/styles.css";
+import '@mantine/core/styles.layer.css';
+import 'mantine-datatable/styles.css';
 
 const preview: Preview = {
   parameters: {
