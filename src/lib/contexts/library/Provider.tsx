d85bf4e7279b19a6bb062e277aec1e3c95ffd5ae
Phil Denhoff <phil@denhoff.ca>
2024-04-01 01:38:44 -0700

    bug: Do not try to re-render the entire component hierarchy as fast as possible
    The way our provider was set up, it tried to re-render ASAP.
    Now, it's much more reasonable: run once. Maybe twice, in strict mode.

--- src/lib/contexts/library/Provider.tsx
+++ src/lib/contexts/library/Provider.tsx
@@ -1,44 +1,55 @@
 import { settings, waitForSettings } from "@/stores/settings";
-import { useEffect, useState } from "react";
-import { DEFAULT_CONTEXT_VALUE, LibraryContext, LibraryContextType, LibraryState } from "./context";
+import { useCallback, useEffect, useState } from "react";
+import {
+	DEFAULT_CONTEXT_VALUE,
+	LibraryContext,
+	LibraryContextType,
+	LibraryState,
+} from "./context";
 import { initClient } from "@/lib/library/libraryCommsManager";
 
 interface LibraryProviderProps {
 	children: React.ReactNode;
 }
 export const LibraryProvider = ({ children }: LibraryProviderProps) => {
-	const [context, setContext] = useState<LibraryContextType>(DEFAULT_CONTEXT_VALUE);
+	const [context, setContext] = useState<LibraryContextType>(
+		DEFAULT_CONTEXT_VALUE,
+	);
+
+	const initializeLibrary = useCallback(async () => {
+		await waitForSettings();
+		const calibreLibraryPath = await settings.get("calibreLibraryPath");
+		const options = {
+			libraryPath: calibreLibraryPath,
+			libraryType: "calibre",
+			connectionType: "local",
+		} as const;
+		const client = await initClient({
+			...options,
+			libraryType: "calibre",
+		});
+		setContext({
+			library: client,
+			loading: false,
+			error: null,
+			state: LibraryState.ready,
+		});
+	}, []);
 
 	useEffect(() => {
-		void (async (): Promise<void> => {
-			await waitForSettings();
-			const calibreLibraryPath = await settings.get("calibreLibraryPath");
-			const options = {
-				libraryPath: calibreLibraryPath,
-				libraryType: "calibre",
-				connectionType: "local",
-			} as const;
-			const client = await initClient({
-				...options,
-				libraryType: "calibre",
-			});
-			setContext({
-				library: client,
-				loading: false,
-				error: null,
-        state: LibraryState.ready
-			});
-		})();
+		initializeLibrary().catch(() => {
+			console.error("Failed to init library");
+		});
 
 		return () => {
 			setContext({
 				library: null,
 				loading: false,
 				error: new Error("Library context was shut down"),
-        state: LibraryState.error
+				state: LibraryState.error,
 			});
 		};
-	});
+	}, [initializeLibrary]);
 
 	return (
 		<LibraryContext.Provider value={context}>
