550b41b2695006aebe84d858b81c10bab1b699e1
Phil Denhoff <phil@denhoff.ca>
2024-03-31 22:55:11 -0700

    feat: Basic AddBook form + story, not hooked up.
    I missed Storybook.

---
+++ src/components/organisms/AddBookForm.stories.tsx
@@ -0,0 +1,52 @@
+import { LibraryAuthor } from "@/bindings";
+import { theme } from "@/lib/theme";
+import { MantineProvider } from "@mantine/core";
+import type { Meta, StoryFn, StoryObj } from "@storybook/react";
+import { AddBookForm } from "./AddBookForm";
+
+const MantineForStorybook = (Story: StoryFn) => {
+	return (
+		<MantineProvider theme={theme} forceColorScheme="dark">
+			<Story />
+		</MantineProvider>
+	);
+};
+
+const meta: Meta<typeof AddBookForm> = {
+	component: AddBookForm,
+	decorators: [MantineForStorybook],
+};
+
+export default meta;
+type Story = StoryObj<typeof AddBookForm>;
+
+const AUTHORS: LibraryAuthor[] = [
+	{
+		name: "Robert Pattison",
+		sortable_name: "Pattison, Robert",
+		id: "gid/author/1234",
+	},
+	{
+		name: "Frank Herbert",
+		sortable_name: "Herbert, Frank",
+		id: "gid/author/4321",
+	},
+];
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+	render: () => (
+		<AddBookForm
+			initial={{
+				authorList: ["Robert Pattison"],
+				title: "The Batman",
+			}}
+			authorList={AUTHORS.map((author) => author.name)}
+			onSubmit={(data) => console.log("Book data added", data)}
+		/>
+	),
+};
---
+++ src/components/organisms/AddBookForm.tsx
@@ -0,0 +1,51 @@
+import { MultiSelectCreatable } from "@/lib/components/ui/Multiselect/Multiselect";
+import { Button, Stack, TextInput } from "@mantine/core";
+import { Form, useForm } from "@mantine/form";
+
+interface AddBookForm {
+	title: string;
+	authorList: string[];
+}
+
+export interface AddBookFormProps {
+	initial: AddBookForm;
+	authorList: string[];
+	onSubmit?: (formData: AddBookForm) => void;
+}
+
+export const AddBookForm = ({
+	initial,
+	authorList,
+	onSubmit,
+}: AddBookFormProps) => {
+	const form = useForm({
+		initialValues: initial,
+	});
+
+	return (
+		<Form
+			form={form}
+			onSubmit={() => {
+				if (onSubmit) {
+					onSubmit(form.values);
+				}
+			}}
+		>
+			<Stack gap={"lg"}>
+				<TextInput label="Title" {...form.getInputProps("title")} />
+				<MultiSelectCreatable
+					label="Authors"
+					placeholder="Search or add author"
+					selectOptions={authorList}
+					onCreateSelectOption={(list, payload) =>
+						console.log({ list, payload })
+					}
+					{...form.getInputProps("authorList")}
+				/>
+				<Button variant="primary" fullWidth type="submit">
+					Add book
+				</Button>
+			</Stack>
+		</Form>
+	);
+};
