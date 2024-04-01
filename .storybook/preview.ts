22df19cc8a6af89e8e06eb0bcc4f6a00b9167c14
Phil Denhoff <phil@denhoff.ca>
2024-04-01 01:22:22 -0700

    cleanup: remove ctp from storybook background options

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,34 +1,21 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from "@storybook/react";
 
 // import "../src/routes/styles.css"
 // import "../src/routes/semantic.css"
 import "../src/styles.css";
 import "@mantine/core/styles.css";
-import '@mantine/core/styles.layer.css';
-import 'mantine-datatable/styles.css';
+import "@mantine/core/styles.layer.css";
+import "mantine-datatable/styles.css";
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-    backgrounds: {
-      default: 'catppuccin latte',
-      values: [
-        {
-          name: 'catppuccin latte',
-          value: '#ccd0da',
-        },
-        {
-          name: 'catppuccin macchiato',
-          value: '#363a4f',
-        },
-      ],
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
 };
 
 export default preview;
