030f402ad68a32b0bff1bd2f6a68b1a17c396d5c
Phil Denhoff <phil@denhoff.ca>
2024-04-03 22:21:35 -0700

    cleanup: remove unused Svelte UI components

--- src/lib/components/forms/Row.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-</script>
-
-<div class="flex flex-row gap-8 items-center w-full mb-4">
-	<slot />
-</div>
--- src/lib/components/ui/dialog/index.ts
+++
@@ -1,9 +0,0 @@
-import Title from "./dialog-title.svelte";
-import Portal from "./dialog-portal.svelte";
-import Footer from "./dialog-footer.svelte";
-import Header from "./dialog-header.svelte";
-import Overlay from "./dialog-overlay.svelte";
-import Content from "./dialog-content.svelte";
-import Description from "./dialog-description.svelte";
-
-export { Title, Portal, Footer, Header, Overlay, Content, Description };
