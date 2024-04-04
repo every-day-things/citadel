1c95e1e5a0e0b3313db7b4be79208ac0012453f0
Phil Denhoff <phil@denhoff.ca>
2024-04-03 22:42:02 -0700

    lint: ignore errors we can't otherwise fix

--- src/lib/services/library/_internal/libraryCommsManager.ts
+++ src/lib/services/library/_internal/libraryCommsManager.ts
@@ -6,6 +6,9 @@ export const initClient = (options: Options): Promise<Library> => {
 		case "calibre":
 			return initCalibreClient(options);
 		default:
+			// The type here is `never` but we're dealing with an unexpected issue
+			// at runtime.
+			// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
 			throw new Error(`Unknown library type: ${options.libraryType}`);
 	}
 };
