2ad15cc61a6a073149a24ef9f178111b6fbf1aa0
Phil Denhoff <phil@denhoff.ca>
2024-04-03 22:39:04 -0700

    refactor: use alias for paths

--- src/lib/services/library/_internal/adapters/calibre.ts
+++ src/lib/services/library/_internal/adapters/calibre.ts
@@ -4,7 +4,7 @@ import {
 	type ImportableBookMetadata,
 	type LibraryBook,
 	type RemoteFile,
-} from "../../../../../bindings";
+} from "@/bindings";
 import type {
 	Library,
 	LocalConnectionOptions,
