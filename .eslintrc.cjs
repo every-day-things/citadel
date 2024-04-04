a54508203ba6c3a9473523274ec085154d6cd711
Phil Denhoff <phil@denhoff.ca>
2024-04-04 00:49:08 -0700

    Add eslint-plugin-paths as lint check

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,36 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  settings: {
-    react: { version: 'detect' },
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts', 'src/bindings.ts'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  }
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	settings: {
+		react: { version: "detect" },
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended-type-checked",
+		"plugin:@typescript-eslint/stylistic-type-checked",
+		"plugin:react-hooks/recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+	],
+	ignorePatterns: [
+		"dist",
+		".eslintrc.cjs",
+		"vite.config.ts",
+		"src/bindings.ts",
+	],
+	parser: "@typescript-eslint/parser",
+	plugins: ["react-refresh", "eslint-plugin-paths"],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+		"paths/alias": "error",
+	},
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		project: ["./tsconfig.json", "./tsconfig.node.json"],
+		tsconfigRootDir: __dirname,
+	},
+};
