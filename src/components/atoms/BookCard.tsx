c4b5ce831fec6f96169c6b23bf95a6e29f2a8e2d
Phil Denhoff <phil@denhoff.ca>
2024-04-14 16:03:04 -0700

    Book sidebar uses Context

--- src/components/atoms/BookCard.tsx
+++ src/components/atoms/BookCard.tsx
@@ -1,19 +1,28 @@
+import React, { useCallback } from "react";
 import { LibraryBook } from "@/bindings";
 import { Card, Center, Image } from "@mantine/core";
-import React, { useCallback } from "react";
 import { shortenToChars } from "./BookAsCover";
 import styles from "./BookCard.module.css";
 
+type BookAction = (bookId: LibraryBook["id"]) => void;
+
+interface BookActions {
+	onViewBook: BookAction;
+}
+
+interface BookCard {
+	book: LibraryBook;
+	actions: BookActions;
+}
+
 export const BookCard = React.memo(function BookCard({
 	book,
-	onBookOpen,
-}: {
-	book: LibraryBook;
-	onBookOpen: (bookId: LibraryBook["id"]) => void;
-}) {
-	const onCoverPress = useCallback(() => {
-		onBookOpen(book.id);
-	}, [book.id, onBookOpen]);
+	actions: { onViewBook },
+}: BookCard) {
+	const onCoverTouch = useCallback(() => {
+		onViewBook(book.id);
+	}, [onViewBook, book]);
+
 	return (
 		<>
 			<Card m="xs" flex={1}>
@@ -26,11 +35,12 @@ export const BookCard = React.memo(function BookCard({
 								fit="contain"
 								src={book.cover_image?.url}
 								alt={book.title}
-								onPointerDown={onCoverPress}
+								onPointerDown={onCoverTouch}
 							/>
 						) : (
 							<div
 								className={styles.coverPlaceholder}
+								onPointerDown={onCoverTouch}
 								style={{
 									backgroundColor: `#${Math.floor(
 										Math.random() * 16777215,
