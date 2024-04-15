93e15cf29104bcb4a7707954d24090b1b1797ddd
Phil Denhoff <phil@denhoff.ca>
2024-04-14 17:45:47 -0700

    [refactor]: extract Book Cover component, use in Book Details

---
+++ src/components/atoms/BookCover.tsx
@@ -0,0 +1,37 @@
+import { Image } from "@mantine/core";
+import { LibraryBook } from "@/bindings";
+import { shortenToChars } from "./BookAsCover";
+import { HTMLAttributes } from "react";
+import styles from "./BookCover.module.css";
+
+export const BookCover = ({
+	book,
+	...props
+}: { book: LibraryBook } & HTMLAttributes<HTMLDivElement>) => {
+	if (book.cover_image?.url !== undefined) {
+		return (
+			<Image
+				h={200}
+				w="auto"
+				fit="contain"
+				src={book.cover_image.url}
+				alt={book.title}
+				{...props}
+			/>
+		);
+	}
+
+	return (
+		<div
+			className={styles.coverPlaceholder}
+			{...props}
+			style={{
+				backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(
+					16,
+				)}`,
+			}}
+		>
+			{shortenToChars(book.title, 50)}
+		</div>
+	);
+};
