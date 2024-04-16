04b1a0f96638fde5f8cd13a45688773915ab9484
Phil Denhoff <phil@denhoff.ca>
2024-04-16 01:02:35 -0700

    cleanup: remove Svelte code + related fns

--- src/components/atoms/BookAsCover.ts
+++ src/components/atoms/BookAsCover.ts
@@ -1,7 +1,6 @@
 import { open } from "@tauri-apps/api/shell";
 import type { LibraryBook } from "../../bindings";
 import { libraryClient } from "../../stores/library";
-import { DeviceType } from "@/lib/services/library";
 
 export const openBookInDefaultApp = (book: LibraryBook) => {
 	const bookAbsPath = libraryClient().getDefaultFilePathForBook(book.id);
@@ -10,25 +9,8 @@ export const openBookInDefaultApp = (book: LibraryBook) => {
 		return;
 	}
 
-	open(bookAbsPath);
-};
-
-export const getBookDownloadUrl = (book: LibraryBook): string | undefined => {
-	const bookAbsPath = libraryClient().getDefaultFilePathForBook(book.id);
-	if (!bookAbsPath) {
-		console.error("Cannot download book", book);
-		return;
-	}
-
-	return bookAbsPath;
+	open(bookAbsPath).catch(console.error);
 };
 
 export const shortenToChars = (str: string, maxChars: number) =>
 	str.length > maxChars ? `${str.substring(0, maxChars)}...` : str;
-
-export const sendToDevice = async (book: LibraryBook, devicePath: string) => {
-	libraryClient().sendToDevice(book, {
-		type: DeviceType.externalDrive,
-		path: devicePath,
-	});
-};
