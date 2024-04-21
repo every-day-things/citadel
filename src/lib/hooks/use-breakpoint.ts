5b511fef21914128ea266ca8894e76dd9d2f07e2
Phil Denhoff <phil@denhoff.ca>
2024-04-21 09:19:04 -0700

    formatting

--- src/lib/hooks/use-breakpoint.ts
+++ src/lib/hooks/use-breakpoint.ts
@@ -1,21 +1,25 @@
 import { useMantineTheme } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 
-export function useBreakpoint(minBreakpoint = 'sm', exact = false) {
-  const theme = useMantineTheme();
+export function useBreakpoint(minBreakpoint = "sm", exact = false) {
+	const theme = useMantineTheme();
 
-  // biome-ignore lint/suspicious/noImplicitAnyLet: <explanation>
-let  query;
-  if (exact) {
-    // Exact breakpoint: min-width of this breakpoint and max-width of the next one (if it exists)
-    const nextBreakpointIndex = Object.keys(theme.breakpoints).indexOf(minBreakpoint) + 1;
-    const nextBreakpointKey = Object.keys(theme.breakpoints)[nextBreakpointIndex];
-    const maxQuery = nextBreakpointKey ? ` and (max-width: calc(${theme.breakpoints[nextBreakpointKey]} - 1px)` : '';
-    query = `(min-width: ${theme.breakpoints[minBreakpoint]}) ${maxQuery}`;
-  } else {
-    // Min-width query for "minBreakpoint or larger"
-    query = `(min-width: ${theme.breakpoints[minBreakpoint]})`;
-  }
+	let query: string;
+	if (exact) {
+		// Exact breakpoint: min-width of this breakpoint and max-width of the next one (if it exists)
+		const nextBreakpointIndex =
+			Object.keys(theme.breakpoints).indexOf(minBreakpoint) + 1;
+		const nextBreakpointKey = Object.keys(theme.breakpoints)[
+			nextBreakpointIndex
+		];
+		const maxQuery = nextBreakpointKey
+			? ` and (max-width: calc(${theme.breakpoints[nextBreakpointKey]} - 1px)`
+			: "";
+		query = `(min-width: ${theme.breakpoints[minBreakpoint]}) ${maxQuery}`;
+	} else {
+		// Min-width query for "minBreakpoint or larger"
+		query = `(min-width: ${theme.breakpoints[minBreakpoint]})`;
+	}
 
-  return useMediaQuery(query);
+	return useMediaQuery(query);
 }
