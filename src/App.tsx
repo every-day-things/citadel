dcd7d51c1eae968203f43ab11b0a640276116358
Phil Denhoff <phil@denhoff.ca>
2024-04-24 23:20:41 -0700

    feat: onboarding selects existing calibre library

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { LibraryProvider } from "$lib/contexts/library";
 import { SettingsProvider } from "$lib/contexts/settings";
 import { theme } from "$lib/theme";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { settings } from "./stores/settings";
 
 import { routeTree } from "./routeTree.gen";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { FirstTimeSetup } from "./components/pages/firstTimeSetup";
 
 const router = createRouter({
 	routeTree,
@@ -20,14 +21,23 @@ declare module "@tanstack/react-router" {
 
 export const App = () => {
 	const [libraryPath, setLibraryPath] = useState<string | null>(null);
+	const updateLibraryPath = useCallback(async () => {
+		const libPath = await settings.get("calibreLibraryPath");
+		setLibraryPath(libPath);
+	}, []);
 	useEffect(() => {
 		settings.subscribe((settings) => {
-			if (settings !== undefined) setLibraryPath(settings.calibreLibraryPath);
+			if (settings !== undefined && settings.calibreLibraryPath.length > 0)
+				setLibraryPath(settings.calibreLibraryPath);
 		});
 	}, []);
 
 	if (libraryPath === null) {
-		return null;
+		return (
+			<MantineProvider theme={theme} defaultColorScheme="auto">
+				<FirstTimeSetup onLibraryPathPicked={updateLibraryPath} />
+			</MantineProvider>
+		);
 	}
 
 	return (
