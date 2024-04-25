b566da0c11455239fdc9d0ad2447dbc71a5a450d
Phil Denhoff <phil@denhoff.ca>
2024-04-24 23:09:53 -0700

    fix: wacky formatting error fixed by breaking up union type

--- src/lib/contexts/library/context.ts
+++ src/lib/contexts/library/context.ts
@@ -9,37 +9,42 @@ export enum LibraryState {
 	starting = 4,
 }
 
+interface CtxClosed {
+	library: null;
+	loading: false;
+	error: Error | null;
+	state: LibraryState.closed;
+}
+interface CtxStarting {
+	library: null;
+	loading: false;
+	error: null;
+	state: LibraryState.starting;
+}
+interface CtxReady {
+	library: Library;
+	loading: false;
+	error: null;
+	state: LibraryState.ready;
+}
+interface CtxLoading {
+	library: null;
+	loading: true;
+	error: null;
+	state: LibraryState.loading;
+}
+interface CtxError {
+	library: null;
+	loading: false;
+	error: Error;
+	state: LibraryState.error;
+}
 export type LibraryContextType =
-	| {
-			library: null;
-			loading: false;
-			error: Error | null;
-			state: LibraryState.closed;
-	  }
-	| {
-			library: null;
-			loading: false;
-			error: null;
-			state: LibraryState.starting;
-	  }
-	| {
-			library: Library;
-			loading: false;
-			error: null;
-			state: LibraryState.ready;
-	  }
-	| {
-			library: null;
-			loading: true;
-			error: null;
-			state: LibraryState.loading;
-	  }
-	| {
-			library: null;
-			loading: false;
-			error: Error;
-			state: LibraryState.error;
-	  };
+	| CtxClosed
+	| CtxStarting
+	| CtxReady
+	| CtxLoading
+	| CtxError;
 
 export const DEFAULT_CONTEXT_VALUE: LibraryContextType = {
 	library: null,
