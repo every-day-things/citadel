cbdf3da105f4bebc9eeeb9072e68d9b6e642fb7e
Phil Denhoff <phil@denhoff.ca>
2024-04-24 23:20:41 -0700

    delete prettier (whew!)

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import("prettier").Config} */
-const config = {
-	plugins: ['prettier-plugin-svelte'],
-	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
-}
-
-export default config
