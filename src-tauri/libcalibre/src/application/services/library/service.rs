f513db711ade0dd70caa5682d3a38fb122818e16
Phil Denhoff <phil@denhoff.ca>
2024-04-26 21:45:55 -0700

    Sanitize file & folder names to avoid issues saving & syncing files

--- src-tauri/libcalibre/src/application/services/library/service.rs
+++ src-tauri/libcalibre/src/application/services/library/service.rs
@@ -1,3 +1,4 @@
+use sanitise_file_name::sanitise;
 use std::error::Error;
 use std::path::{Path, PathBuf};
 use std::result::Result;
@@ -39,15 +40,19 @@ impl std::fmt::Display for LibSrvcError {
 impl std::error::Error for LibSrvcError {}
 
 pub fn gen_book_folder_name(book_name: &String, book_id: i32) -> String {
-    "{title} ({id})"
-        .replace("{title}", book_name)
-        .replace("{id}", &book_id.to_string())
+    sanitise(
+        &"{title} ({id})"
+            .replace("{title}", book_name)
+            .replace("{id}", &book_id.to_string()),
+    )
 }
 
 fn gen_book_file_name(book_title: &String, author_name: &String) -> String {
-    "{title} - {author}"
-        .replace("{title}", book_title)
-        .replace("{author}", author_name)
+    sanitise(
+        &"{title} - {author}"
+            .replace("{title}", book_title)
+            .replace("{author}", author_name),
+    )
 }
 
 pub struct LibraryService<BS, AS, FS, BFS>
