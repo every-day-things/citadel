a52aeab9edcf231cae617b74d14ca8e3a7da482a
Phil Denhoff <phil@denhoff.ca>
2024-06-23 14:59:32 -0700

    Backend provides HTML book descriptions

--- src-tauri/libcalibre/src/entities/book_aggregate.rs
+++ src-tauri/libcalibre/src/entities/book_aggregate.rs
@@ -5,14 +5,17 @@ pub struct BookWithAuthorsAndFiles {
     pub book: Book,
     pub authors: Vec<Author>,
     pub files: Vec<BookFile>,
+    /// An partially HTML-formatted description of the book. User-editable.
+    pub book_description_html: Option<String>,
 }
 
 impl BookWithAuthorsAndFiles {
-    pub fn new(book: Book, authors: Vec<Author>, files: Vec<BookFile>) -> Self {
+    pub fn new(book: Book, authors: Vec<Author>, files: Vec<BookFile>, description: Option<String>) -> Self {
         Self {
             book,
             authors,
             files,
+            book_description_html: description
         }
     }
 }
