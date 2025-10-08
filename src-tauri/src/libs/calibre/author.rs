b1cb0ef7021683ccf903a2c27301de59880bbbbf
Phil Denhoff <phil@denhoff.ca>
2025-10-08 09:07:51 -0700

    feat: backend supports creating authors

--- src-tauri/src/libs/calibre/author.rs
+++ src-tauri/src/libs/calibre/author.rs
@@ -1,4 +1,5 @@
-use libcalibre::client::CalibreClient;
+use libcalibre::{client::CalibreClient, dtos::author::NewAuthorDto};
+use serde::{Deserialize, Serialize};
 
 use crate::book::LibraryAuthor;
 
@@ -12,6 +13,27 @@ impl From<&libcalibre::Author> for LibraryAuthor {
     }
 }
 
+#[derive(Serialize, specta::Type, Deserialize, Clone)]
+pub struct NewAuthor {
+    name: String,
+    sortable_name: Option<String>,
+}
+
+impl From<&NewAuthor> for NewAuthorDto {
+    fn from(author: &NewAuthor) -> Self {
+        let sortable_name = match &author.sortable_name {
+            Some(name) => name.clone(),
+            None => author.name.clone(),
+        };
+
+        NewAuthorDto {
+            full_name: author.name.clone(),
+            sortable_name,
+            external_url: None,
+        }
+    }
+}
+
 pub fn list_all(library_root: String) -> Vec<LibraryAuthor> {
     match libcalibre::util::get_db_path(&library_root) {
         None => vec![],
@@ -20,6 +42,24 @@ pub fn list_all(library_root: String) -> Vec<LibraryAuthor> {
 
             client
                 .list_all_authors()
+                .map(|author_list| author_list.iter().map(LibraryAuthor::from).collect())
+                .unwrap_or_default()
+        }
+    }
+}
+
+pub fn create_authors(library_root: String, new_authors: Vec<NewAuthor>) -> Vec<LibraryAuthor> {
+    match libcalibre::util::get_db_path(&library_root) {
+        None => vec![],
+        Some(database_path) => {
+            let mut client = CalibreClient::new(database_path);
+            let dtos = new_authors
+                .iter()
+                .map(NewAuthorDto::from)
+                .collect::<Vec<NewAuthorDto>>();
+
+            client
+                .create_authors(dtos)
                 .map(|author_list| author_list.iter().map(LibraryAuthor::from).collect())
                 .unwrap_or_default()
         }
